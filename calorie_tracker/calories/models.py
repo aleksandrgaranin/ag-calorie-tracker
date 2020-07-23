91680732e05aa4eab0c8dcf42ca9e13c605b7997
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-07-23 12:04:49 -0700

    created calories app + index.html + Select list

---
+++ calorie_tracker/calories/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Food(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    protein = models.FloatField()
+    fat = models.FloatField()
+    calories = models.IntegerField()
+
+
+    
---
+++ calorie_tracker/calories/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Food
+
+# Create your views here.
+
+def index(request):
+    foods = Food.objects.all()
+    return render(request,'calories/index.html',{'foods':foods})
